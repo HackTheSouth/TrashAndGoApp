71a174f7e6028ba402dfcf65ed55038d3d32a780
L-Gamble <lg4g18@soton.ac.uk>
2020-02-09 02:51:14 +0000

    Loading screen

---
+++ app/src/main/java/com/hackthesouth2020/LoadingScreen.java
@@ -0,0 +1,31 @@
+package com.hackthesouth2020;
+
+
+import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.os.Bundle;
+
+public class LoadingScreen extends AppCompatActivity {
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.loading_screen);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openHomePage();
+            }
+        });
+    }
+
+    public void openHomePage() {
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
+    }
+
+}
